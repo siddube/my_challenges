a857a5a996869c686e43304dae0f0d070c8bb5f4
Siddube <prajwal.be.21@gmail.com>
2024-05-21 15:35:51 +0530

    add month routes and views

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('month/<int:month>', views.month_by_number),
+    path('month/<str:month>', views.month)
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
+
+monthly_challenges_list = {
+    'january': 'Chill and meditate',
+    'february': 'Chill and play guitar',
+    'march': 'Chill and play games',
+    'april': 'Chill and meditate',
+    'may': 'Chill and play guitar',
+    'june': 'Chill and play games',
+    'july': 'Chill and meditate',
+    'august': 'Chill and play guitar',
+    'september': 'Chill and play games',
+    'october': 'Chill and meditate',
+    'november': 'Chill and play guitar',
+    'december': 'Chill and play games',
+    }
 
 # Create your views here.
 def index(request):
     return HttpResponse('My Monthly Challenges')
+
+def month_by_number(request, month):
+    return HttpResponse('Challenges for ' + str(month))
+
+def month(request, month):
+    challenge_text = ''
+    try:
+        challenge_text = monthly_challenges_list[month]
+        return HttpResponse(challenge_text)
+    except:
+        return HttpResponseNotFound('Not a month, so chill')
