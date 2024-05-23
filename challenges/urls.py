b4ce48e23c1672151732800d49fbe56efc502812
Siddube <prajwal.be.21@gmail.com>
2024-05-23 08:34:02 +0530

    add navigation partials

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('month/<int:month>', views.month_by_number),
     path('month/<str:month>', views.month, name='month-challenges')
 ]
